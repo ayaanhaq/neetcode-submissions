868e94d2c5368ca047c54f003c864e3048fafef9
ayaanhaq <ayaanhaq1106@gmail.com>
2026-07-08 15:46:24 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        c={}
+        for i in s:
+            if i not in c:
+                c[i]=0
+            c[i]+=1
+        d={}
+        for j in t:
+            if j not in d:
+                d[j]=0
+            d[j]+=1
+        if c==d:
+            return True
+        else:
+            return False
